8a67e980a006a7b02d9e5987946a4a54f37be330
root <root@shipmnts.asia-south1-c.c.shipmnts.internal>
2021-08-31 17:10:32 +0000

    server setup changes

--- twitter-backend/app-v2.py
+++ twitter-backend/app-v2.py
@@ -4,7 +4,7 @@ import os
 
 
 
-from v2.db import created_app
+from v2.db import *
 
 #Import all blueprints here
 # from v2.blueprints.external_api_blueprint import external_api
@@ -13,8 +13,6 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # only ERRORS are printed
 warnings.filterwarnings('ignore')
 from flask_cors import CORS
 import json
-import numpy as np
-import boto3
 from flask_mysqldb import MySQL
 
 
@@ -33,6 +31,6 @@ mysql = MySQL()
 
 
 if __name__ == '__main__':
-    #app.run(port=80)
-    app.run(host='0.0.0.0', port=80, debug=True)
+    #app.run(port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
